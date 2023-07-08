9ebc72e067c948cf6e9689f6ae9c7f49e8d364e2
bethune-bryant <bryantstutoringservice@gmail.com>
2023-07-08 14:55:08 -0500

    Add tests add speedtest dep.

--- test/test_trace_test.py
+++ test/test_trace_test.py
@@ -1,3 +1,5 @@
+from subprocess import CompletedProcess
+
 from internet_troubleshooter.ping_test import PingResult
 from internet_troubleshooter.trace_test import TraceResult
 
@@ -6,3 +8,36 @@ def test_TraceResult():
     x = TraceResult([PingResult(ip="1.1.1.1", packetLoss=10)])
     assert x.pingResults[0].ip == "1.1.1.1"
     assert x.pingResults[0].packetLoss == 10
+
+
+def test_execute_test(mocker, capsys):
+    test_output = '''TEST STRING'''
+
+    mocker.patch('subprocess.run',
+                 return_value=CompletedProcess(None,
+                                               returncode=0,
+                                               stdout=test_output))
+
+    x = TraceResult.execute_test("8.8.8.8")
+    assert test_output == x
+    captured = capsys.readouterr()
+    assert captured.out == ""
+    assert captured.err == ""
+
+
+def test_execute_test_error(mocker, capsys):
+    test_output = '''TEST STRING'''
+    error_output = '''ERROR STRING'''
+
+    mocker.patch('subprocess.run',
+                 return_value=CompletedProcess(None,
+                                               returncode=1,
+                                               stdout=test_output,
+                                               stderr=error_output))
+
+    x = TraceResult.execute_test("8.8.8.8")
+    assert x is None
+    captured = capsys.readouterr()
+    assert captured.out == ""
+    assert "ERROR:" in captured.err
+    assert error_output in captured.err
