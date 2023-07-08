c0ae16040fd2df6bcb87e56cc5ababfca31f1ee6
bethune-bryant <bryantstutoringservice@gmail.com>
2023-07-08 11:11:37 -0500

    linting fixes

--- test/test_ping_test.py
+++ test/test_ping_test.py
@@ -1,6 +1,7 @@
 from subprocess import CompletedProcess
 from internet_troubleshooter.ping_test import PingResult
 
+
 def test_PingResult():
     x = PingResult(ip="1.1.1.1", packetLoss=10)
     assert x.ip == "1.1.1.1"
